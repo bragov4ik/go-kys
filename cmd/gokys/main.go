b78dabd5c0f68f2b4ea2393f66b170eabebac082
Feyd-Rauth <atreides.pasha@yandex.ru>
2021-09-26 16:24:12 +0300

    Function for recursive scanning implemented

--- cmd/gokys/main.go
+++ cmd/gokys/main.go
@@ -2,7 +2,32 @@ package main
 
 import (
 	_ "github.com/bragov4ik/go-kys/pkg/kys"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+
+}
+
+func WalkMatch(root, pattern string) ([]string, error) {
+	var matches []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
+			return err
+		} else if matched {
+			matches = append(matches, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
 }
