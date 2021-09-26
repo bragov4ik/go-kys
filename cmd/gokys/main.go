0d92b701ad41e3e7331faf77c352b2336a157ad9
Feyd-Rauth <atreides.pasha@yandex.ru>
2021-09-26 16:49:29 +0300

    cli added

--- cmd/gokys/main.go
+++ cmd/gokys/main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	_ "github.com/bragov4ik/go-kys/pkg/kys"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
+	var fileNames string
+	var directory string
 
+	flag.StringVar(&fileNames, "f", "", "Files to count metrics for")
+	flag.StringVar(&directory, "d", "", "Name of the directory for recursive scanning")
+	flag.Parse()
+
+	var files []string
+	var err error
+
+	if directory == "" {
+		files = strings.Split(fileNames, " ")
+	} else {
+		files, err = WalkMatch(directory, "*.go")
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(files)
 }
 
 func WalkMatch(root, pattern string) ([]string, error) {
