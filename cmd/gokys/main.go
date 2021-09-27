213f2430e382ffb943b2883932f1ec45b15a53f5
Feyd-Rauth <69901637+Feyd-Rauth@users.noreply.github.com>
2021-09-27 12:07:00 +0300

    CLI added (#2)
    * Function for recursive scanning implemented
    
    * cli added
    
    * readme updated
    
    * cli fixed
    
    * Revert "readme updated"
    
    This reverts commit fd56f494abead1f614d965cf33fc7bea442a9d7d.
    
    * imports optimized

--- cmd/gokys/main.go
+++ cmd/gokys/main.go
@@ -1,8 +1,58 @@
 package main
 
 import (
-	_ "github.com/bragov4ik/go-kys/pkg/kys"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	var files []string
+
+	for _, v := range args {
+		info, err := os.Stat(v)
+
+		if os.IsNotExist(err) {
+			log.Fatal("File does not exist.")
+		}
+
+		if info.IsDir() {
+			files, err = WalkMatch(v, "*.go")
+		} else {
+			files = append(files, v)
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println(files)
+}
+
+func WalkMatch(root, pattern string) ([]string, error) {
+	var matches []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
+			return err
+		} else if matched {
+			matches = append(matches, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
 }
