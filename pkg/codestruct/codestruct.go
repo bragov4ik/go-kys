52c3806eceeb0e70d85507ca0414d5604b74faa2
Feyd-Rauth <69901637+Feyd-Rauth@users.noreply.github.com>
2021-10-09 15:51:55 +0300

    Add calculation of code structure complexity (#16)
    * Add calculation of code structure complexity

---
+++ pkg/codestruct/codestruct.go
@@ -0,0 +1,39 @@
+package codestruct
+
+import (
+	"go/ast"
+)
+
+type Weights struct {
+	Func      uint `xml:"func"`
+	Struct    uint `xml:"struct"`
+	Interface uint `xml:"interface"`
+}
+
+type Metric struct {
+	Config Weights
+	Comp   uint
+}
+
+func (m *Metric) ParseNode(n ast.Node) {
+	m.Comp += getCodeStructComp(n, &m.Config)
+}
+
+func getCodeStructComp(n ast.Node, cfg *Weights) uint {
+	comp := uint(0)
+
+	switch n.(type) {
+	case *ast.StructType:
+		comp = cfg.Struct
+	case *ast.FuncDecl:
+		comp = cfg.Func
+	case *ast.InterfaceType:
+		comp = cfg.Interface
+	}
+
+	return comp
+}
+
+func (m Metric) Finish() float64 {
+	return float64(m.Comp)
+}
