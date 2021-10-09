9748c365bf79dd5c858a64bd138a329c5ed10477
Feyd-Rauth <69901637+Feyd-Rauth@users.noreply.github.com>
2021-10-09 16:29:49 +0300

    Add test for code structure complexity (#17)
    * Add test for code structure complexity

---
+++ pkg/codestruct/codestruct_test.go
@@ -0,0 +1,39 @@
+package codestruct
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
+)
+
+func TestCodeStructComp(t *testing.T) {
+	src := `package main
+
+			type foo struct {}
+			type bar struct {}
+			
+			type foobar interface {}
+			
+			func f1(){}
+			func f2(){}
+			
+			func main(){}`
+
+	file, err := parser.ParseFile(token.NewFileSet(), "", src, parser.ParseComments)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m := Metric{Config: Weights{Struct: 1, Func: 1, Interface: 1}}
+
+	ast.Inspect(file, func(n ast.Node) bool {
+		m.ParseNode(n)
+		return true
+	})
+	got := m.Comp
+	want := uint(6)
+	if want != got {
+		t.Errorf("TestCodeStructComp got %v, want %v", got, want)
+	}
+}
